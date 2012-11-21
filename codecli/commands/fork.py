d5345409192cb6fff8087c0bd72bfdfe90842a9e
Qiangning Hong <hongqn@douban.com>
2012-11-21 18:04:28 +0800

    specify dir to clone

--- codecli/commands/fork.py
+++ codecli/commands/fork.py
@@ -7,14 +7,15 @@ def populate_argument_parser(parser):
     username = getuser()
     parser.add_argument('upstream', help="name of upstream repo [e.g. dae]")
     parser.add_argument('origin', help="name of my fork [e.g. dae_hongqn]")
+    parser.add_argument('dir', help="directory to clone")
     parser.add_argument('--username', default=username,
                         help="douban unified account name [default: %(default)s]")
 
 
 def main(args):
     name = args.upstream
-    check_call(['git', 'clone', git_url(args.origin), name])
-    with cd(name):
+    check_call(['git', 'clone', git_url(args.origin), args.dir])
+    with cd(args.dir):
         check_call(['git', 'remote', 'add', 'upstream', git_url(name)])
         check_call(['git', 'config', 'user.email',
                     '%s@douban.com' % args.username])
