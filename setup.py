7f9f43630baea539776efd9b090fdb69070e25dc
Qiangning Hong <hongqn@douban.com>
2012-12-21 17:35:33 +0800

    better setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,56 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
+
+# package meta info
+NAME = "CodeCLI"
+VERSION = "0.1"
+DESCRIPTION = "A command line tool for CODE"
+AUTHOR = "Qiangning Hong"
+AUTHOR_EMAIL = "hongqn@gmail.com"
+LICENSE = "BSD"
+URL = ""
+KEYWORDS = ""
+CLASSIFIERS = []
+
+# package contents
+MODULES = []
+PACKAGES = find_packages(exclude=['tests.*', 'tests', 'examples.*', 'examples'])
+ENTRY_POINTS = """
+[console_scripts]
+code = codecli:main
+"""
+
+# dependencies
+INSTALL_REQUIRES = []
+TESTS_REQUIRE = ['nose']
+TEST_SUITE = 'nose.collector'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_long_description(filename):
+    path = os.path.join(here, filename)
+    if os.path.exists(path):
+        return open(path).read()
+    return ""
 
 setup(
-    name = "CodeCLI",
-    version = "0.1",
-    description = "Command line interface for code.dapps.douban.com",
-    packages = ['codecli'],
-    entry_points = {
-        'console_scripts': [
-            'code = codecli:main',
-        ],
-    },
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=read_long_description('README.rst'),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    url=URL,
+    keywords=KEYWORDS,
+    classifiers=CLASSIFIERS,
+    py_modules=MODULES,
+    packages=PACKAGES,
+    install_package_data=True,
+    zip_safe=False,
+    entry_points=ENTRY_POINTS,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    test_suite=TEST_SUITE,
 )
