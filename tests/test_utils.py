0bcb61ed09fa33170e10b6cab822981fe9354dd4
Qiangning Hong <hongqn@douban.com>
2013-04-28 23:12:31 +0800

    support pullreq on a pullreq

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 from subprocess import check_call
 from nose.tools import eq_
+from mock import patch
 import codecli.utils as M
 from tests.utils import mkdtemp
 
@@ -13,3 +14,17 @@ def test_get_branches_should_return_a_list_of_branch_names():
 
         branches = M.get_branches()
     eq_(branches, ['master'])
+
+
+def test_get_remote_repo_url_should_work():
+    with patch.object(M, 'getoutput') as mock_getoutput:
+        mock_getoutput.return_value = """\
+origin  http://code.dapps.douban.com/testrepo.git (fetch)
+origin  http://code.dapps.douban.com/testrepo.git (push)
+upstream    http://code.dapps.douban.com/testrepo.git (fetch)
+upstream    http://code.dapps.douban.com/testrepo.git (push)
+"""
+
+        repourl = M.get_remote_repo_url('origin')
+    eq_(repourl, 'http://code.dapps.douban.com/testrepo')
+ 
