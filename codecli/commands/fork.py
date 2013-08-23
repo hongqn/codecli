81288c3653099f0cc7255cce77cc5949b0d811fe
satoru <satorulogic@gmail.com>
2013-08-23 12:33:13 +0800

    code fork时默认clone到仓库同名目录

--- codecli/commands/fork.py
+++ codecli/commands/fork.py
@@ -1,14 +1,20 @@
-from codecli.utils import check_call, repo_git_url, cd, merge_config
+from codecli.utils import (check_call, repo_git_url, cd, merge_config,
+                           print_log)
 
 
 def populate_argument_parser(parser):
     parser.add_argument('upstream', help="name of upstream repo [e.g. dae]")
     parser.add_argument('origin', help="name of my fork [e.g. hongqn/dae]")
-    parser.add_argument('dir', help="directory to clone")
+    parser.add_argument('dir', nargs='?', help="directory to clone")
 
 
 def main(args):
     name = args.upstream
+
+    if not args.dir:
+        args.dir = name.rsplit('/')[-1]
+        print_log("Destination dir is not specified, will use {}".format(args.dir))
+
     check_call(['git', 'clone', repo_git_url(args.origin), args.dir])
     with cd(args.dir):
         merge_config()
