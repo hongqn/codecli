85fba6518550a969b12e4e903cbc6d97aa7c5775
WooParadog <guohaochuan@gmail.com>
2014-08-19 11:44:59 +0800

    Add remote branch parameter

--- codecli/commands/sync.py
+++ codecli/commands/sync.py
@@ -4,8 +4,9 @@ from codecli.utils import get_current_branch_name, merge_with_base
 def populate_argument_parser(parser):
     parser.add_argument('-r', '--rebase', action='store_true',
                         help="rebase with upstream")
+    parser.add_argument('-b', '--base', help="Branch to rebase on")
 
 
 def main(args):
     branch = get_current_branch_name()
-    merge_with_base(branch, rebase=args.rebase)
+    merge_with_base(branch, rebase=args.rebase, remote_branch=args.base)
